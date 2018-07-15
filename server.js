9b98d8dbfd61f7dfe898d0d9c494412c9c2ca4ea
Kal Academy <info@kalacademy.org>
2018-07-15 16:47:17 -0700

    user to tables

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');const posts = require('./routes/api/posts');
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Db config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDb
+mongoose
+.connect(db)
+.then(() => console.log('MongoDb connected.'))
+.catch(err => console.log(err));
 
 //Let's write out first route
 app.get('/', (req, res) => res.send('Hello'));
