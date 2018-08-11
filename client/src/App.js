447681ff40757ef12d6d9bf31e9adb9ecb3f3714
Kal Academy <info@kalacademy.org>
2018-08-11 10:15:34 -0700

    Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
@@ -8,9 +10,12 @@ import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import './App.css';
 
+
+
 class App extends Component {
   render() {
     return (
+      <Provider store={ store }>
       <Router>
       <div className="App">
         <Navbar />
@@ -22,6 +27,7 @@ class App extends Component {
         <Footer />
       </div>
       </Router>
+      </Provider>
     );
   }
 }
